e399dde20cabc1601b03ca1cfa042cbbe658fdd3
nithin8702 <nithin8702@gmail.com>
2017-05-20 12:36:57 +0530

    updated

--- RNDSystemsUI/src/app/app.module.ts
+++ RNDSystemsUI/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Angular2FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
